8bfc1c821dd49dddfdfa47567387b65444a517a7
Michael Bestas <mkbestas@lineageos.org>
2025-04-24 10:19:33 +0000

    mt6768-common: Shim libnvram/libsysenv with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -129,6 +129,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
             ;;
+        vendor/lib/libnvram.so | vendor/lib/libsysenv.so | vendor/lib64/libnvram.so | vendor/lib64/libsysenv.so)
+       [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
