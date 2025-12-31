1ccbfc408cbb47443627d481dd491b948c797f55
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-31 12:49:11 +0100

    mt6768-common: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,7 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 
 # Lineage Health
 ifneq ($(wildcard hardware/*/interfaces/health),)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
